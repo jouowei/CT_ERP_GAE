978977bb35ce4b171e10988e7cff9ac7dab41f6c
jouowei <jouowei@gmail.com>
2018-06-29 14:48:04 +0800

    fix huayuan query

--- static/js/huayuan/huayuan_controller_query.js
+++ static/js/huayuan/huayuan_controller_query.js
@@ -52,6 +52,7 @@ myApp.controller('queryForm', function ($scope, $http, $mdDialog, myService, myF
             }
         });
         hidePleaseWait();
+        disableUI(false);
     }
     
 });
@@ -70,26 +71,33 @@ myApp.controller('resultForm', function ($scope, $http, $mdDialog, myService, my
     //資料驗證
     $scope.validateNcal = function (rawdata, ev) {
         $scope.orderPrice = 0;
+        let boolPass = true;
         for (i = 0; i < rawdata.length; i++) {
             let editedShip = rawdata[i];
             if (editedShip.delivery_fee <= 0) {
 				myService.showAlert($mdDialog, ev, "第" + (i+1) + "行錯誤", "運費不得為0");
                 $scope.show.SubmitBtn = false;
                 disableUI(false);
+                boolPass = false
                 break;
             } else if (parseInt(editedShip.shipUnits) > parseInt(editedShip.delivery_fee)) {
 				myService.showAlert($mdDialog, ev, "第" + (i+1) + "行錯誤", "運費低於件數");
                 $scope.show.SubmitBtn = false;
                 disableUI(false);
+                boolPass = false
                 break;
             } else if (editedShip.comment.length == 0 ) {
                 myService.showAlert($mdDialog, ev, "注意", '修改紀錄必須填寫備註。');
                 $scope.show.SubmitBtn = false;
                 disableUI(false);
+                boolPass = false
                 break;
             }
             $scope.show.SubmitBtn = true;
             disableUI(true);
+        }
+        if (!boolPass) {
+            return boolPass;
         }
         if ($scope.show.SubmitBtn) {
             for (let j = 0; j < $scope.viewDatas.data.length; j++) {
@@ -103,6 +111,7 @@ myApp.controller('resultForm', function ($scope, $http, $mdDialog, myService, my
                             editedShip.shipUnits = initShip.shipUnits;
                             $scope.show.SubmitBtn = false;
                             disableUI(false);
+                            boolPass = false
                             break;
                         }
                     }
@@ -110,15 +119,24 @@ myApp.controller('resultForm', function ($scope, $http, $mdDialog, myService, my
                         myService.showAlert($mdDialog, ev, "注意", '修改紀錄必須填寫備註。');
                         $scope.show.SubmitBtn = false;
                         disableUI(false);
+                        boolPass = false
                         break;
                     }
                 }
+            }
+            if (!boolPass) {
+                return boolPass;
             }
         };
     };
 
     //送出資料
-    $scope.submitForm = function () {
+    $scope.submitForm = function (ev) {
+        if (!$("input[type=button]")[0].disabled){
+            if(!$scope.validateNcal($scope.viewDatas.data, ev)){
+                return;
+            }
+        }
         //資料整理
         var arrFinalData = [];
         $scope.viewDatas.data.forEach(function (x, index) {
